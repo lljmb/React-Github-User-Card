4cbdfea8ae9d0d58effaa50ee8e5e1311da1a8fb
lljmb <brown.jessm@icloud.com>
2021-01-12 19:45:52 -0500

    completed project

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import Followers from './Followers'
+
+class App extends React.Component {
+  // set the state of the users
+  state = {
+    users: [], 
+    followers: []
+  }
+
+  // retrieve user from API
+  componentDidMount(){
+    axios
+    .get('https://api.github.com/users/lljmb')
+    .then(res => {
+      this.setState({
+        users: res.data 
+      })
+    })
+    .catch(error => {
+      console.log('error:', error)
+    })
+  }
+
+  componentDidUpdate(){
+    axios
+    .get('https://api.github.com/users/lljmb/followers')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        followers: res.data
+      })
+    })
+    .catch(error => {
+      console.log('error:', error)
+    })
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  // view
+  render(){
+   return(
+    <div className='App'>
+        <img src={this.state.users.avatar_url} alt='user profile'/>
+        <h2>{this.state.users.name}</h2>
+        <a href={this.state.users.html_url}>{this.state.users.html_url}</a>
+        <p>{this.state.users.bio}</p>
+  
+    <div className='container'>
+    <h1>Hello Friends!</h1>
+    
+    <Followers followers={this.state.followers} />
     </div>
-  );
+    </div>
+   )
+  }
+
+
 }
+
 
 export default App;
